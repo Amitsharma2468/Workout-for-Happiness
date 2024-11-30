29af6be4d2e81c78b497c04679922d6591f35d86
yasinbashar <107603927+yasinbashar@users.noreply.github.com>
2024-11-30 22:51:34 +0600

    backend created

--- server/app.js
+++ server/app.js
@@ -1,10 +1,21 @@
-import express from 'express';
+import express from "express";
+import dotenv from "dotenv";
+import errorHandler from "./middleware/errorHandler.js";
+import connectDb from "./config/dbConnection.js";
+import userRoutes from "./routes/userRoute.js";  
+
+dotenv.config();
+
+connectDb();
+
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send("okay");
-});
+const port = process.env.PORT || 5000;
 
-app.listen(4000, () => {
-    console.log("Server running on port 4000");
+app.use(express.json()); 
+app.use("/api/users", userRoutes);  
+app.use(errorHandler);
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
 });
